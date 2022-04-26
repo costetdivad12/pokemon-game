a178a53f487e1d7c80061fcd91619c910ec4b30f
costetdivad12 <david.costet@iebem.edu.mx>
2022-04-26 15:57:26 -0500

    agregando las opciones para saber que pokemon es

--- src/helpers/getOptions.js
+++ src/helpers/getOptions.js
@@ -1,10 +1,39 @@
+import Pokemonapi from '../api/Pokemonapi'
+
 const Pokemons=()=>{
     const pokemonsarr= Array.from(Array(650 ));
    return  pokemonsarr.map((_,index)=>index+1);
 }
 
-const getPokemonoptions= ()=>{
-  console.log( Pokemons());
+const getPokemonoptions=async ()=>{
+  const mixisPokemon= Pokemons().sort(()=>Math.random() - 0.5);
+  
+  const pokemons= await getPokemonNames(mixisPokemon.splice(0,4));
+  // console.table(pokemons);
+  return pokemons
+}
+const getPokemonNames=async ( [a,b,c,d] = [])=>{
+
+  const promiseArr=[
+    Pokemonapi.get(`/${a}`),
+    Pokemonapi.get(`/${b}`),
+    Pokemonapi.get(`/${c}`),
+    Pokemonapi.get(`/${d}`)
+  ]
+
+  const [p1,p2,p3,p4] =await Promise.all(promiseArr)
+  //  console.log(p1,p2,p3,p4);
+
+ return [
+   {name:p1.data.name,id:p1.data.id},
+   {name:p2.data.name,id:p2.data.id},
+   {name:p3.data.name,id:p3.data.id},
+   {name:p4.data.name,id:p4.data.id},
+ ]
+
+//  const respuesta =await Pokemonapi.get('/4')
+
+//  console.log(respuesta.data.name,respuesta.data.id);
 }
 
 export default getPokemonoptions
--- src/pages/PokemonPage.vue
+++ src/pages/PokemonPage.vue
@@ -1,20 +1,33 @@
 <template>
-<h1>Quien es ese pokemon?</h1>
- <pokemonpicture :pokemonId="4" :imgpoke="true"/>
- <pokemonoptions/>
+  <h1>Quien es ese pokemon?</h1>
+  <pokemonpicture :pokemonId="4" :imgpoke="true" />
+  <pokemonoptions  :pokemons="pokemonarr"/>
 </template>
 
 <script>
-import Pokemonoptions from '../components/Pokemonoptions.vue'
-import Pokemonpicture from '../components/Pokemonpicture.vue'
-import getPokemonoptions from '@/helpers/getOptions'
-console.log(getPokemonoptions());
+import Pokemonoptions from "../components/Pokemonoptions.vue";
+import Pokemonpicture from "../components/Pokemonpicture.vue";
+import getPokemonoptions from "@/helpers/getOptions";
+
+// console.log(getPokemonoptions());
 export default {
   components: { Pokemonpicture, Pokemonoptions },
-
+  data() {
+    return {
+      pokemonarr:[],
+      pokemon:null
+    }
+  },
+  methods:{
+    async mixPokemonsArr(){
+       this.pokemonarr = await getPokemonoptions()
+    }
+  },
+  mounted(){
+    this.mixPokemonsArr()
+  }
 }
 </script>
 
 <style>
-
 </style>
